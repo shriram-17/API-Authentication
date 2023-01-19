6840ff4bf102775a07c32fdc757c6b01237d413a
shriram-17 <76253109+shriram-17@users.noreply.github.com>
2023-01-19 15:51:07 +0530

    completed User And Register Route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const router =require('express').Router()
 const User =require('../models/User')
 const bcrypt =require('bcrypt')
+const joi =require('@hapi/joi')
+const {registervalidation, loginvalidation} =require('../validation')
+const   JWT = require('jsonwebtoken')
 
 router.get('/', (req,res)=>{
     res.send("You Have Entered the authenication Room")
@@ -9,6 +12,20 @@ router.get('/', (req,res)=>{
 
 router.post("/register", async (req,res)=>{
    
+    const {error} =registervalidation(req.body)
+    if(error) 
+    {
+        return res.status(400).send(error.details[0].message)
+    }
+    // checking the user already exists
+ 
+        
+    const emailExist=await User.findOne({email:req.body.email})
+    if(emailExist)
+    {
+        return res.status(400).send("Email Already Exists")
+    }
+    
     const salt= await bcrypt.genSalt(10)  
     const hashedpassword =await bcrypt.hash(req.body.password,salt)
     const user = new User({
@@ -22,6 +39,30 @@ router.post("/register", async (req,res)=>{
         res.status(200).send(savedUser) 
     } catch (error) {
         res.status(400).send({status:"Faliure", msg:err })
+    }
+})
+
+router.post('/login',async (req,res) => {
+    const{error} =loginvalidation(req.body)
+    if(error)
+    {
+        return res.status(400).send(error.details[0].message)
+    }
+    
+    const user=await User.findOne({email:req.body.email})
+    if(!user)
+    {
+        return res.status(400).send("Invalid Email")
+    }
+   
+    const validpass =await bcrypt.compare(req.body.password,user.password)
+    if(validpass)
+    {
+        return res.status(200).send(`${user.name} logged In`)
+    }
+    else
+    {
+        return res.status(400).send('Invalid Password')
     }
 })
 
